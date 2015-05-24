8b94fe3e2f41a4e496a49e7e5f066c2b6d4df166
emretetik96 <et2406@columbia.edu>
2015-05-23 23:56:09 -0400

    Added functions to app.py

--- app.py
+++ app.py
@@ -3,7 +3,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello World!'
+  return 'Home'
+
+@app.route('/about')
+def about():
+	return 'About'
+
+@app.route('/projects')
+def projects():
+	return 'Projects'
+
+@app.route('/writing_journalism')
+def writing_journalism():
+	return 'Writing/Journalism'
+
+@app.route('/contact')
+def contact():
+	return 'contact'
+
+@app.errorhandler(404)
+def not_found(error):
+	return 'The page you are looking for does not exist...'
 
 if __name__ == '__main__':
     app.run()
